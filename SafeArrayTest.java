de843221b4e6540f86e7eb18c749fa5fc3fc49ab
hmin1022 <121006387+hmin1022@users.noreply.github.com>
2023-03-19 04:24:56 +0900

    [upload] ch06 example
    Java ch06 : AccessModifierTest01, Box, Date, MyCounter, Rectangle, Student, Test [ update ]

---
+++ SafeArrayTest.java
@@ -0,0 +1,14 @@
+package ch06;
+
+public class SafeArrayTest {
+  public static void main(String[] args) {
+    SafeArray sarr = new SafeArray(3);
+
+    for(int i = 0; i <= sarr.length; i++) {
+      sarr.put(i, (i+1)*10);
+    }
+    for(int i = 0; i < sarr.length; i++) {
+      System.out.println("["+i+"]:" + sarr.get(i) + " ");
+    }
+  }
+}
